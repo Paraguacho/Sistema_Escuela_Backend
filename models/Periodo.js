b82ffe925252c70b3003581181bf6ac2663ecda8
Paraguacho <sebastiansoq03@hotmail.com>
2025-11-03 19:52:34 -0700

    padreHijo / Periodo Scheme

---
+++ models/Periodo.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const periodoSchema = new Schema({
+    nombre: {
+        type: String, //Primer parcial - semestre etc
+        required: true,
+        trim: true
+    },
+    fechaInicio: {
+        type: Date,
+        required: true
+    },
+    fechaFinal: {
+        type: Date,
+        required: true
+    },
+    fechaLimiteCalificaciones:{
+        type: Date,
+        required: true //Fecha limite para que suban las calificaciones.
+    }
+},{
+    timestamps: true
+});
+
+
+const Periodo = mongoose.model('Periodo', usuarioSchema);
+module.exports = Periodo;
---
+++ models/padreHijo.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+/**
+ * Schema de relacion, 
+ */
+const padreHijoSchema = new mongoose.Schema({
+    padre: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario',
+        required: true
+    },
+    hijo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Estudiante',
+        required: true
+    }
+});
+
+//Index para ligar padre con  hijo
+padreHijoSchema.index({ padre: 1, hijo: 1 }, { unique: true });
+const PadreHijo = mongoose.model('PadreHijo', padreHijoSchema);
+module.exports = PadreHijo;
