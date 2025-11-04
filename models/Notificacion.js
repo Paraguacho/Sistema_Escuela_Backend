9cb30a44817d5b2fb739fa50d4eb8a5110f80250
Paraguacho <sebastiansoq03@hotmail.com>
2025-11-03 20:25:49 -0700

    Notificacion schema

---
+++ models/Notificacion.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const notificacionSchema = new mongoose.Schema({
+    usuarioDestino: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Usuario', 
+        required: true
+    },
+    tipo: {
+        type: String,
+        enum: ['Bajo rendimiento','Tarea por vencer','Nuevo Mensaje','Tarea Avalada', 'Calificar Tarea'],
+        required: true
+    },
+    contenido: {
+        type: String,
+        required: true
+    },
+    estado: {
+        type: String,
+        enum: ['Leida', 'No leida'], 
+        default: 'No leida'       
+    },
+    //Referencia para direccionar al usuario(lo podriamos usar despues)
+    refId: { 
+        type: mongoose.Schema.Types.ObjectId
+    }
+}, {
+    timestamps: true
+});
+
+const Notificacion= mongoose.model('Notificacion', notificacionSchema);
+module.exports = Notificacion;
